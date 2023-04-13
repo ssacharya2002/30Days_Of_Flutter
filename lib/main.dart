96ca8951920d38bd4acf2d27a309dd11e7366e14
ssacharya <ssacharya2002@gmail.com>
2023-04-13 20:19:47 +0530

    day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:demo/pages/login_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'home_page.dart';
+import 'pages/home_page.dart';
+import 'pages/login_page.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -9,10 +12,23 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-
     return MaterialApp(
-        home: HomePage(),
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple
+      ),
+
+
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        // primarySwatch: Colors.green  
+      ),
+      initialRoute: "/home",
+      routes:{
+        "/":(context) => LoginPage(),
+        "/home" : (context) => HomePage(),
+        "/Loginpage" : (context) =>  LoginPage()
+      } ,
     );
-}
+  }
 }
